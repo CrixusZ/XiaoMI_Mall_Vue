a54abd292a5bf9d96d19245929fbb1ae736ee885
CrixusZ <673659295@qq.com>
2020-06-27 22:17:42 +0800

    baseURL配置

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,36 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axiox from 'axios'
+import VueAxios from 'vue-axios'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import env from './env'
 
+// 根据前端的跨域方式做调整
+axiox.defaults.baseURL = '/api'
+axiox.defaults.timeout = 8000
+// 请求拦截
+axiox.interceptors.request.use((config) => {
+  // 在发送请求之前做些什么
+  return config
+}, (error) => {
+  // 对请求错误做些什么
+  return Promise.reject(error)
+})
+// 接口的错误拦截
+axiox.interceptors.response.use((response) => {
+  const res = response.data
+  if (res.status === 0) {
+    return res.data
+  } else if (res.status === 10) {
+    window.location.href = '/#/login'
+  } else {
+    alert(res.msg)
+  }
+})
+
+Vue.use(VueAxios, axiox)
 Vue.config.productionTip = false
 
 new Vue({
--- src/storage/index.ts
+++ src/storage/index.ts
@@ -4,17 +4,37 @@
 const STORAGE_KEY = 'mall';
 export default {
   // 存储值 ()
-  setItem() {
-
+  setItem(key:string, value:string, module_name?:string) {
+    if (module_name){
+      let val = this.getItem(module_name);
+      val[key] = value;
+      this.setItem(module_name, val);
+    }else{
+      let val = this.getStorage();
+      val[key] = value;
+      window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify(val));
+    }
   },
-  // 获取值
-  getItem() {
-
+  // 获取某一个模块下面的属性user下面的userName
+  getItem(key:string, module_name?:string) {
+    if (module_name){
+      let val:{} = this.getItem(module_name);
+      if(val) return val[key];
+    }
+    return this.getStorage()[key];
   },
+  // 获取全部的值
   getStorage() {
-
+    return JSON.parse(window.sessionStorage.getItem(STORAGE_KEY) || '{}')
   },
-  clear() {
-    
+  clear(key:string, module_name:string) {
+    let val = this.getStorage();
+    if (module_name){
+      if (!val[module_name])return;
+      delete val[module_name][key];
+    }else{
+      delete val[key];
+    }
+    window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify(val));
   }
 }
