63cc8ce8f091840b57c9d901ffae34204ea489fc
CrixusZ <673659295@qq.com>
2020-06-27 08:29:40 +0800

    跨域配置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+    >
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from 'axios'
+import jsonp from 'jsonp'
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      age: 30,
+      data: {}
+    }
+  },
+  mounted () {
+    const url = '/?c=be'
+    axios.get(url).then(res => {
+      console.log(res)
+    })
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    jsonp(url, (res) => {
+      console.log(res)
+      this.data = res
+    })
   }
 }
 </script>
